c72da81247a81900e40f98f46fd5b90b43873ad2
fusekd3 <fnm2248fnm10@naver.com>
2025-08-20 15:28:09 +0900

    Fix firebase dependencies and update frontend

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvjwu9JGYrWPKjIawfIv9zB49mCYcRUpY",
@@ -13,5 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app); 
+export const auth = getAuth(app); 
